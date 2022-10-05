27e56935de3f5eba0a8f6d32910b26d7c171b81a
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-10-06 01:42:40 +0200

    by using prop, add value 'title' in  Row.js
    Comment in both files (App.js & Row.js)

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -2,12 +2,13 @@ import React from 'react'
 import './App.css';
 import Row from './Row'
 
+/*add two row components and customise them by adding titles of each genre */
 function App() {
   return (
     <div className="App">
       <h1>Let successfully complete this netflix clone in Jesus name</h1>
       <Row title="NETFLIX ORIGINALS" />
-      <Row title="Trending Now" /> /** add two row components and customise them by adding titles of each genre */
+      <Row title="Trending Now" /> 
     </div>
   );
 }
--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-function Row() {
+function Row({ title }) {
     return(
         <div>
-
+            <h2>{title}</h2>
+            {/**container=> posters */}
         </div>
     )
 }
