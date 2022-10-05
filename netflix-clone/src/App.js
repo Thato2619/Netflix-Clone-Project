e9b6057bc8e6ca883b5a984e4539ff9d7695135b
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-10-06 01:17:19 +0200

    App.js: don't forget to comment

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <h1>Let successfully complete this netflix clone in Jesus name</h1>
       <Row title="NETFLIX ORIGINALS" />
-      <Row title="Trending Now" />
+      <Row title="Trending Now" /> /** add two row components and customise them by adding titles of each genre */
     </div>
   );
 }
