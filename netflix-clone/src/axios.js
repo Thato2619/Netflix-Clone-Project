67d0dba4b2eeae3a4555e9d3efba4628f36f9ae3
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-10-06 01:07:15 +0200

    import axios module and create a variable(instance)
    the axios has a function that helps creates a base url request to the movie database

---
+++ netflix-clone/src/axios.js
@@ -0,0 +1,15 @@
+import axios from "axios"; //import axios module
+
+/** add function that creates base url to make request to the movie database */
+const instance = axios.create({
+    baseURL:"https://api.themoviedb.org/3"
+});
+
+//helps appends the instance of movie or site
+instance.get('/foo-bar');
+
+/** see outcome of the following:
+ * https://api.themoviedb.org/3/foo-bar
+ */
+
+export default instance;
