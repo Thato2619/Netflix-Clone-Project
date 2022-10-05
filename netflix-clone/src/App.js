ed6878a1d92412fd8bc5f0c4ab102645c9a033ec
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-10-06 01:16:02 +0200

    App.js: add two row components and customise by adding title of genres

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <h1>Let successfully complete this netflix clone in Jesus name</h1>
+      <Row title="NETFLIX ORIGINALS" />
+      <Row title="Trending Now" />
     </div>
   );
 }
