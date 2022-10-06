d4070d6365f37d6effa5ce46640e4424847b4dd6
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-10-06 10:02:55 +0200

    add a header tag within banner function

--- netflix-clone/src/Banner.js
+++ netflix-clone/src/Banner.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 function Banner() {
-  return (
-    <div>Banner</div>
-  )
+    return (
+        <header>
+            
+        </header>
+    )
 }
 
 export default Banner
