0e1058ca402f5f33ef9b2d4df4e4f7158e6ba500
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-10-06 15:43:59 +0200

    add img tag with the netflix avator

--- netflix-clone/src/Navbar.js
+++ netflix-clone/src/Navbar.js
@@ -3,11 +3,18 @@ import React from 'react'
 function Navbar() {
     return (
     <div className= "nav">
+        
         <img 
             className="nav_logo"
             src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
             alt="Netflix Logo"
         />
+        <img
+            className='nav_avator'
+            src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+            alt="Netflix Avator"
+        />
+
     </div>
     )
 }
