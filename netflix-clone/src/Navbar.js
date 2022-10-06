9b99891d0faaf95657e7374b6645150cd326997d
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-10-06 15:27:26 +0200

    get link to Netflix logo and add className = nav_logo

--- netflix-clone/src/Navbar.js
+++ netflix-clone/src/Navbar.js
@@ -3,7 +3,11 @@ import React from 'react'
 function Navbar() {
     return (
     <div className= "nav">
-
+        <img 
+            className="nav_logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+            alt="Netflix Logo"
+        />
     </div>
     )
 }
