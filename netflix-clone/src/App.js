1f645d75fd6e19178bce5aed201f7d50ad10f334
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-10-06 06:18:10 +0200

    copy the Row component and rename the genre of movies and also fetch the url requests of the corresponding genres

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -10,6 +10,12 @@ function App() {
       <h1>Let successfully complete this netflix clone in Jesus name</h1>
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} /> 
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} /> 
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} /> 
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} /> 
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} /> 
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} /> 
+      <Row title="Documantaries" fetchUrl={requests.fetchDocumantaries} /> 
     </div>
   );
 }
