6b7c0b46b1eb914d83eea5480129ea87e1525dce
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-10-06 03:06:10 +0200

    Row.js: add useEffect function to help run code
    Comment the information as you go.

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,11 +1,17 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
 function Row({ title }) {
-    const[movies, setMovies] = useState([])
+    const[movies, setMovies] = useState([]);
+
+    //A snippet of code which runs based on a specific condition/variable
+    useEffect(() => {
+        //
+    }, [])
     return(
         <div>
             {/** prop ~ title */}
             <h2>{title}</h2>
+
             {/**container=> posters */}
         </div>
     )
