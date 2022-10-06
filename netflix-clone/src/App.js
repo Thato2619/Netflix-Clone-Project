2f333b053d39294e65c69812c472f88da2407786
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-10-06 02:12:37 +0200

    add fetchUrl prop values to fetch movie genres

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import './App.css';
 import Row from './Row'
-import re
+import requests from './requests';
 
 /*add two row components and customise them by adding titles of each genre */
 function App() {
   return (
     <div className="App">
       <h1>Let successfully complete this netflix clone in Jesus name</h1>
-      <Row title="NETFLIX ORIGINALS" />
-      <Row title="Trending Now" /> 
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} /> 
     </div>
   );
 }
