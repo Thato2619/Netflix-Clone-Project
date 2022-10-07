9a3c35bb1742059e42b850814582efa8b68299a2
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-10-07 14:41:24 +0200

    add <h1> tag and add Sign In

--- netflix-clone/src/SignIn.js
+++ netflix-clone/src/SignIn.js
@@ -5,7 +5,12 @@ function SignIn()  {
     <div className='signIn_screen_background'>
         <img className='screen_background' src="" alt="" />
         <div className="signIn_screen_gradient"></div>
-        <div className="signIn_screen_form"></div>
+        <div className="signIn_screen_form">
+            <form>
+                <h1>Sign In</h1>
+
+            </form>
+        </div>
     </div>
   )
 }
