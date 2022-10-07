393c1656d485c664162c2743ee829b1dad572b93
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-10-07 18:00:36 +0200

    format indent  Signin.js & Signin.css files

--- netflix-clone/src/SignIn.js
+++ netflix-clone/src/SignIn.js
@@ -1,21 +1,24 @@
-import React {useState}from "react";
+import React, { useState } from "react";
 import "./SignIn.css";
 function SignIn() {
-    //add variable of [App,setApp] to link App.js
-    const [App, setApp] = useState(false);
+  //add variable of [App,setApp] to link App.js
+  const [App, setApp] = useState(false);
   return (
     <div className="signIn_screen_background">
-      <img className="screen_logo" src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="" />
+      <img
+        className="screen_logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt=""
+      />
       <div className="signIn_screen_gradient"></div>
       <div className="signIn_screen_form">
         <form>
           <h1>Sign In</h1>
           <input placeholder="Email" type="email" />
           <input placeholder="Password" type="password" />
-            <button 
-                onClick={() => setApp(true)}
-                type="submit">Sign In
-            </button>
+          <button onClick={() => setApp(true)} type="submit">
+            Sign In
+          </button>
 
           <h4>
             <span className="signIn_screen_grey">New to Netflix?</span>
