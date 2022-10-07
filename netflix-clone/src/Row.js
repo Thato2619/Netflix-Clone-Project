dc201dff8a9bb6759a285afdfd7c1426950b396f
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-10-07 07:29:22 +0200

    import youtuube from react-youtube to Row.js file

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import axios from "./axios";
 import "./Row.css";
+import Youtube from "react-youtube"
 
 //baseUrl for the lack of functionlaity 
 const baseUrl = "https://image.tmdb.org/t/p/original/";
