6eb91e51c5dfebf5ccb10594c81af3cfbd14813a
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-10-07 14:03:59 +0200

    change classname to signIn_screen_background
    style the background image by using position, height, background
    
    add comments too

--- netflix-clone/src/SignIn.js
+++ netflix-clone/src/SignIn.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 function SignIn()  {
   return (
-    <div className='screen'>
-        <img className='screen_background' src="https://assets.nflxext.com/ffe/siteui/vlv3/afc06103-4d6a-4236-b496-34b671a7e9ba/5b679305-b790-4639-8a60-0881064fe5e6/ZA-en-20221003-popsignuptwoweeks-perspective_alpha_website_medium.jpg" />
+    <div className='signIn_screen_background'>
+        <img className='screen_background' src="" alt="" />
+
     </div>
   )
 }
