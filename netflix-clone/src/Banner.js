4269174e82c0f0970e95f0f9ec75d0033cdc2e1e
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-10-07 09:05:00 +0200

    format ident both Banner.js and Row.js files

--- netflix-clone/src/Banner.js
+++ netflix-clone/src/Banner.js
@@ -1,59 +1,61 @@
-import React, { useEffect, useState } from 'react'
-import axios from "./axios"
-import requests from './Requests';
-import './Banner.css';
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
+import requests from "./Requests";
+import "./Banner.css";
 function Banner() {
-    const [movie, setMovie] = useState([]);
+  const [movie, setMovie] = useState([]);
 
-    //use useEffect to run code on a condition 
-    useEffect(() => {
-        async function fetchData(){
-            const request = await axios.get(requests.fetchTrending)
-            setMovie(
-                request.data.results[
-                    Math.floor(Math.random() * request.data.results.length - 1)
-                ]
-            );
-            return request;
-        } 
-        fetchData();
-    }, []);
-    console.log(movie);
-
-    //this function truncate helps make description fit in the banner 
-    function truncate (str, n){
-        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  //use useEffect to run code on a condition
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchTrending);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
     }
+    fetchData();
+  }, []);
+  console.log(movie);
 
-    {/** add backgroun image in header*/}
-    return ( 
-        //begin to add background image in 'banner' container
-        //use <style></style>
-        <header className="banner"> 
-        style={{ 
-            backgroundSize: "cover",
-            backgroundImage: `url(
+  //this function truncate helps make description fit in the banner
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
+  {
+    /** add backgroun image in header*/
+  }
+  return (
+    //begin to add background image in 'banner' container
+    //use <style></style>
+    <header className="banner">
+      style=
+      {{
+        backgroundSize: "cover",
+        backgroundImage: `url(
                 "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
             )`,
-            backgroundPosition: "center center" ,//centres the image in background
-        }}
-            <div className="banner_content">
-            {/** title: conditional to look for name of movies*/}
-            <h1 className="banner_title"> 
-                {movie?.title || movie?.name || movie?.original_name}
-            </h1>
-            <div className='banner_buttons'> 
-                <button className="banner_buttons">Play</button>
-                <button className="banner_buttons">My List</button>
-                {/** banner_buttons x2*/}
-            </div>
-            <h1 className="banner_description">{movie?.overview}</h1>
-            {/** banner_description*/}
-            </div>
-            
-            <div className="banner--fadeBottom"></div>
-        </header>
-    )
+        backgroundPosition: "center center", //centres the image in background
+      }}
+      <div className="banner_content">
+        {/** title: conditional to look for name of movies*/}
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner_buttons">
+          <button className="banner_buttons">Play</button>
+          <button className="banner_buttons">My List</button>
+          {/** banner_buttons x2*/}
+        </div>
+        <h1 className="banner_description">{movie?.overview}</h1>
+        {/** banner_description*/}
+      </div>
+      <div className="banner--fadeBottom"></div>
+    </header>
+  );
 }
 
-export default Banner
+export default Banner;
