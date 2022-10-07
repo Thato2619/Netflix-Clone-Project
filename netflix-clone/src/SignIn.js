08ed693109e9f487ef7e4f96a19fca1d4544c9ee
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-10-07 13:15:06 +0200

    add img propperty and add the background image of Netflix

--- netflix-clone/src/SignIn.js
+++ netflix-clone/src/SignIn.js
@@ -3,7 +3,7 @@ import React from 'react'
 function SignIn()  {
   return (
     <div className='screen_background'>
-
+        <img className='' src="https://assets.nflxext.com/ffe/siteui/vlv3/afc06103-4d6a-4236-b496-34b671a7e9ba/5b679305-b790-4639-8a60-0881064fe5e6/ZA-en-20221003-popsignuptwoweeks-perspective_alpha_website_medium.jpg" />
     </div>
   )
 }
