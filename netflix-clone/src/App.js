f9aa7d5ceb0700f0874dbd70d03ebab4dc3276e1
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-10-07 16:03:52 +0200

    format indent all files in the src folder

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,10 @@
-import React from'react';
-import './Row.css';
-import './App.css';
-import Row from './Row';
-import requests from './Requests';
-import Banner from './Banner';
-import Navbar from './Navbar';
+import React from "react";
+import "./Row.css";
+import "./App.css";
+import Row from "./Row";
+import requests from "./Requests";
+import Banner from "./Banner";
+import Navbar from "./Navbar";
 
 /*add two row components and customise them by adding titles of each genre */
 function App() {
@@ -15,18 +15,18 @@ function App() {
       {/** Banner*/}
       <Banner />
 
-      <Row 
-        title="NETFLIX ORIGINALS" 
+      <Row
+        title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
         isLargeRow
-        />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} /> 
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} /> 
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} /> 
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} /> 
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} /> 
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} /> 
-      <Row title="Documantaries" fetchUrl={requests.fetchDocumantaries} /> 
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documantaries" fetchUrl={requests.fetchDocumantaries} />
     </div>
   );
 }
--- netflix-clone/src/SignIn.js
+++ netflix-clone/src/SignIn.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import './SignIn.css'
-function SignIn()  {
+import React from "react";
+import "./SignIn.css";
+function SignIn() {
   return (
-    <div className='signIn_screen_background'>
-        <img className='screen_background' src="" alt="" />
-        <div className="signIn_screen_gradient"></div>
-        <div className="signIn_screen_form">
-            <form>
-                <h1>Sign In</h1>
-                <input placeholder="Email" type="email" />
-                <input placeholder="Password" type="password" />
-                <button type="submit">Sign In</button>
+    <div className="signIn_screen_background">
+      <img className="screen_background" src="" alt="" />
+      <div className="signIn_screen_gradient"></div>
+      <div className="signIn_screen_form">
+        <form>
+          <h1>Sign In</h1>
+          <input placeholder="Email" type="email" />
+          <input placeholder="Password" type="password" />
+          <button type="submit">Sign In</button>
 
-                <h4>
-                    <span className="signIn_screen_grey">New to Netflix?</span>
-                    <span className="signIn_screen_link">Sign Up now</span>
-                </h4>
-            </form>
-        </div>
+          <h4>
+            <span className="signIn_screen_grey">New to Netflix?</span>
+            <span className="signIn_screen_link">Sign Up now</span>
+          </h4>
+        </form>
+      </div>
     </div>
-  ) 
+  );
 }
 
-export default SignIn
+export default SignIn;
