23f795d2ac6106c3d41100ac2ed99f1d76211b21
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-10-07 17:18:09 +0200

    add onClick function so user can be on the landing page(App.js)

--- netflix-clone/src/SignIn.js
+++ netflix-clone/src/SignIn.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React {useState}from "react";
 import "./SignIn.css";
 function SignIn() {
+    //add variable of [App,setApp] to link App.js
+    const [App, setApp] = useState(false);
   return (
     <div className="signIn_screen_background">
       <img className="screen_background" src="" alt="" />
@@ -10,7 +12,10 @@ function SignIn() {
           <h1>Sign In</h1>
           <input placeholder="Email" type="email" />
           <input placeholder="Password" type="password" />
-          <button type="submit">Sign In</button>
+            <button 
+                onClick={() => setApp(true)}
+                type="submit">Sign In
+            </button>
 
           <h4>
             <span className="signIn_screen_grey">New to Netflix?</span>
