9494e851d73cfd6a552956270e053bddbfd371d7
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-10-07 14:10:53 +0200

    create div tag with className: signIn_screen_gradient

--- netflix-clone/src/SignIn.js
+++ netflix-clone/src/SignIn.js
@@ -4,7 +4,7 @@ function SignIn()  {
   return (
     <div className='signIn_screen_background'>
         <img className='screen_background' src="" alt="" />
-
+        <div className="signIn_screen_gradient"></div>
     </div>
   )
 }
